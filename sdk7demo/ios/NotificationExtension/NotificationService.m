b90a4ba1213e415b1a8507e87f07d07b8ff5c8f1
Gabor Vass <gaborvass@users.noreply.github.com>
2021-11-15 16:22:48 +0100

    add checks when building for simulator

--- sdk7demo/ios/NotificationExtension/NotificationService.m
+++ sdk7demo/ios/NotificationExtension/NotificationService.m
@@ -6,23 +6,29 @@
 //
 
 #import "NotificationService.h"
+#if !TARGET_IPHONE_SIMULATOR
 #import <GetSocialNotificationExtension/GetSocialNotificationExtension.h>
-
+#endif
 @interface NotificationService ()
 
+#if !TARGET_IPHONE_SIMULATOR
 @property (nonatomic, strong) GetSocialNotificationRequestHandler *handler;
-
+#endif
 @end
 
 @implementation NotificationService
 
 - (void)didReceiveNotificationRequest:(UNNotificationRequest *)request withContentHandler:(void (^)(UNNotificationContent * _Nonnull))contentHandler {
+#if !TARGET_IPHONE_SIMULATOR
   self.handler = [GetSocialNotificationRequestHandler new];
   [self.handler handleNotificationRequest:request withContentHandler:contentHandler];
+#endif
 }
 
 - (void)serviceExtensionTimeWillExpire {
+#if !TARGET_IPHONE_SIMULATOR
   [self.handler serviceExtensionTimeWillExpire];
+#endif
 }
 
 @end
